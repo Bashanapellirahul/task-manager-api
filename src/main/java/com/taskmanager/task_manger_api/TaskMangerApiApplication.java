53c4bdcabd4fb4bfe785f289507a90efc364585f
RAHUL_BASHANAPELLI <bashanapellirahul@gmail.com>
2026-03-19 15:51:15 -0500

    first commit

---
+++ src/main/java/com/taskmanager/task_manger_api/TaskMangerApiApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.task_manger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMangerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMangerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanager/task_manger_api/TaskMangerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.task_manger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMangerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
